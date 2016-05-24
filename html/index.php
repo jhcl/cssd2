b7fa9ed0096870187f90337494a8e7591876ac85
root <root@leakywiks.leakywiks.xyz>
2016-05-24 11:23:51 +0000

    login toegevoegd

--- html/index.php
+++ html/index.php
@@ -1,15 +1,18 @@
+<?php session_start() ?>
 <?php include "../includes/header.php" ?>
 <?php include "../includes/heading.php" ?>
 
 <div id="registerForm">
   <?php
+    include "../includes/login.php";
     include "../includes/dbconn.php";
     print_r(PDO::getAvailableDrivers());
     //  echo phpinfo();
     $sql = 'SELECT username, password FROM gebruiker';
     foreach ($dbh->query($sql) as $row) {
+      print "<br>";
       print $row['username'] . "\t";
-      print $row['password'] . "\t";
+      print $row['password'];
     }
   ?>
 </div>
