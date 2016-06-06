e2b9896190115b2b1f78a5016f42e76d9fb67df8
jhcl <jhcl@users.noreply.github.com>
2016-06-06 17:34:06 +0200

    added auth checking to download, share and delete

--- html/bookaction.php
+++ html/bookaction.php
@@ -1,34 +1,53 @@
 <?php
 session_start();
+
 require_once('classes/Database.php');
+require_once('classes/User.php');
 $db = new Database();
+$usr = new User(null, $_SESSION['user'], null,null);
+
+if (!isset($_SESSION['user']) || $_SESSION['user'] == '') {
+    header('Location: /index.php');
+}
 
 switch ($_POST['bookaction']) {
 case 'delete': 
-    if ($db->deleteStatement("delete from user_bestandid where bestandid = :id" , array("id"=>$_POST['bestandid'])) &&
-        $db->deleteStatement("delete from comment where fileid = :id", array("id"=>$_POST['bestandid'])) &&
-        $db->deleteStatement("delete from bestand where id = :id" , array("id"=>$_POST['bestandid']))) {
-            unlink(addslashes($_POST['bestandloc']));
+    $id = $_POST['bestandid'];
+    if ($usr->isOwner($id)) {
+        if ($db->deleteStatement("delete from user_bestandid where bestandid = :id" , array("id"=>$id)) &&
+            $db->deleteStatement("delete from comment where fileid = :id", array("id"=>$id)) &&
+            $db->deleteStatement("delete from bestand where id = :id" , array("id"=>$id))) {
+                unlink(addslashes($_POST['bestandloc']));
         }
+    } else {
+        $_SESSION['msg'] = "Only owner can delete this file.";
+    }
     break;
 case 'share': 
-    $db->insertStatement("insert into user_bestandid (username, bestandid) values (:user, :bestandid)",
-        array("user"=>$_POST['username'], "bestandid"=>$_POST['bestandid']));
+    if ($usr->isOwner($_POST['bestandid'])) {
+        $db->insertStatement("insert into user_bestandid (username, bestandid) values (:user, :bestandid)",
+            array("user"=>$_POST['username'], "bestandid"=>$_POST['bestandid']));
+    } else {
+        $_SESSION['msg'] = "Only owner can share this file.";
+    }
     break;
 case 'addComment': 
-    $db->insertStatement("insert into comment (username, date, fileid, comment) values (:user, :date, :fileid, :comment)", 
-        array("user"=>$_SESSION['user'], "date"=>date("Y-m-d H:i:s"), "fileid"=>$_POST['fileid'], "comment"=>$_POST['commentArea']));
+    if ($usr->isOwner($_POST['fileid']) || $usr->isInvited($_POST['fileid'])) {
+        $db->insertStatement("insert into comment (username, date, fileid, comment) values (:user, :date, :fileid, :comment)", 
+            array("user"=>$_SESSION['user'], "date"=>date("Y-m-d H:i:s"), "fileid"=>$_POST['fileid'], "comment"=>$_POST['commentArea']));
+    } else {
+        $_SESSION['msg'] = "Only invited people can comment on this file.";
+    }
     break;
 case 'download': 
-//    if (checkAuth($_SESSION['user'], $_POST['bestandid'])) {
-    $result = $db->selectStatement("select location,name from bestand where id = :id", array("id"=>$_POST['bestandid']));
-    header('Content-type: ' . mime_content_type ($result[0]['location']));
-//    echo $result[0]['name']; echo $result[0]['location'];  die;
-    header('Content-Disposition: attachment; filename="' . $result[0]["name"] . '"');
-    readfile($result[0]['location']);
-//   } else {
-//     $_SESSION['msg'] = "Not authorized";
-//   }
+    if ($usr->isOwner($_POST['bestandid']) || $usr->isInvited($_POST['bestandid'])) {
+        $result = $db->selectStatement("select location,name from bestand where id = :id", array("id"=>$_POST['bestandid']));
+        header('Content-type: ' . mime_content_type ($result[0]['location']));
+        header('Content-Disposition: attachment; filename="' . $result[0]["name"] . '"');
+        readfile($result[0]['location']);
+    } else {
+      $_SESSION['msg'] = "Not authorized";
+    }
     exit;
 }
 header("Location: /hoofdpagina.php");
